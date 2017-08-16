900260f90fe69da48e96df4c874eb98b665e3d88
Nilesh Kasvala <nilesh.kasvala@ordextechnology.com>
2017-08-16 17:27:45 +0530

    Production 1.3(1.4) Version Live on App Store

--- IOSWAITLIST/Kyobee/AppDelegate.h
+++ IOSWAITLIST/Kyobee/AppDelegate.h
@@ -27,13 +27,13 @@
  PWD : jaekim
 */
 
-
+/*
 #define logoUrl @"http://jbossdev-kyobee.rhcloud.com/static/orglogos/" // DEV Envireonment
 #define ServiceUrl @"http://jbossdev-kyobee.rhcloud.com/kyobee/" // DEV Envireonment
 #define ChannelName @"RSNT_GUEST_DEV_" // DEV Envireonment*/
 
 
-/*#define logoUrl @"http://jbossqa-kyobee.rhcloud.com/static/orglogos/" // QA Envireonment
+#define logoUrl @"http://jbossqa-kyobee.rhcloud.com/static/orglogos/" // QA Envireonment
 #define ServiceUrl @"http://jbossqa-kyobee.rhcloud.com/kyobee/" // QA Envireonment
 #define ChannelName @"RSNT_GUEST_QA_" // QA Envireonment*/
 
