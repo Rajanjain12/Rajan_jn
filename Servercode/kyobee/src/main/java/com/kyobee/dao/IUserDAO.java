b1aafaba7ca9c209b68f2d59f91f0f055772fd8a
Krupali Baldaniya <krupali.baldaniya@ordextechnology.com>
2018-05-07 16:23:54 +0530

    commit as of 07/05/2018 - sendSMS popup responsive fixes. - maximum limit validation text change. - sendSMS fix for Ramen-Ultra

--- Servercode/kyobee/src/main/java/com/kyobee/dao/IUserDAO.java
+++ Servercode/kyobee/src/main/java/com/kyobee/dao/IUserDAO.java
@@ -1,9 +1,12 @@
 //created by pampaniya shweta for forgot password
 package com.kyobee.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.kyobee.entity.User;
 import com.kyobee.exception.RsntException;
 
+@Repository
 public interface IUserDAO 
 {
 	public String getUserAuthCode(long userId) throws RsntException;
