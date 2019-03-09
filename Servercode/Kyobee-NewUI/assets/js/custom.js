d92f989e88d31b59f6d7bb7b74eddce24fb6000f
Ruchi <ruchi.vakharia@ordextechnology.com>
2019-03-09 17:55:27 +0530

    committed for pending things in waitlist pending items ---------------- 1. range slider pending 2. responsive pending 3. some small-small things need to be check 4. merge profile design of siddharth still partially incomplete

--- Servercode/Kyobee-NewUI/assets/js/custom.js
+++ Servercode/Kyobee-NewUI/assets/js/custom.js
@@ -7,3 +7,23 @@ $('.activeIcon li a').click(function() {
 
 var ele = localStorage.getItem('activeState');
 $('.activeIcon li:eq(' + ele + ')').find('a').addClass('activeState');
+
+var expanded = false;
+/*checkbox inside dropdown*/
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.className = "showDropdown";
+    expanded = true;
+  } else {
+    checkboxes.className = "hideDropdown";
+    expanded = false;
+  }
+}
+/*input mask for mobile number*/
+document.getElementById('phone').addEventListener('input', function (e) {
+  var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+  e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+});
+/*multiselect*/
+$('.selectpicker').selectpicker();
