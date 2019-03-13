b330cb2e7ef01836911ad2479fe9db02a93bccab
Ruchi <ruchi.vakharia@ordextechnology.com>
2019-03-13 11:58:35 +0530

    committed for new font update

--- Servercode/Kyobee-NewUI/assets/js/custom.js
+++ Servercode/Kyobee-NewUI/assets/js/custom.js
@@ -4,12 +4,10 @@ $('.activeIcon li a').click(function() {
   $(this).addClass("activeState");
   localStorage.setItem('activeState', $(this).parent().index());
 });
-
 var ele = localStorage.getItem('activeState');
 $('.activeIcon li:eq(' + ele + ')').find('a').addClass('activeState');
-
-var expanded = false;
 /*checkbox inside dropdown*/
+var expanded = false;
 function showCheckboxes() {
   var checkboxes = document.getElementById("checkboxes");
   if (!expanded) {
@@ -20,10 +18,18 @@ function showCheckboxes() {
     expanded = false;
   }
 }
+/*show hide password*/
+$(".toggle-password").click(function() {
+  $(this).toggleClass("flaticon-eye-1 flaticon-eye");
+  var input = $($(this).attr("toggle"));
+  if (input.attr("type") == "password") {
+    input.attr("type", "text");
+  } else {
+    input.attr("type", "password");
+  }
+});
 /*input mask for mobile number*/
 document.getElementById('phone').addEventListener('input', function (e) {
   var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
   e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
 });
-/*multiselect*/
-$('.selectpicker').selectpicker();
