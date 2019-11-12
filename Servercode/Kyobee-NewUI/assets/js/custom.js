fd2dcc2f873b0fd05ab21efab898941dee91e450
Ruchi <ruchi.vakharia@ordextechnology.com>
2019-11-12 12:06:12 +0530

    committed for design issues 1. spacing issue 2. responsive thing 3. verification code input design issue

--- Servercode/Kyobee-NewUI/assets/js/custom.js
+++ Servercode/Kyobee-NewUI/assets/js/custom.js
@@ -45,6 +45,28 @@ $(".sidehide").click(function(){
   $("#sidebar-wrapper").css("animation-name","slid-right");
 });
 
+/*verification code auto focus previous and next*/
+function moveOnNext(field) { 
+   var id = field.id;
+   var arr = id.split("_");
+   var incr = parseInt(arr[1]) + 1;
+   var nextFieldID = "code_"+ incr;
+   if (field.value.length >= field.maxLength) { 
+    document.getElementById(nextFieldID).focus(); 
+  } 
+  else{
+    moveOnPrevious(field)
+  }
+}
+function moveOnPrevious(field) { 
+  var id = field.id;
+  var arr = id.split("_");
+  var decr = parseInt(arr[1]) - 1;
+  var prviousFieldID = "code_" + decr;
+  if (field.value.length <= 0) { 
+    document.getElementById(prviousFieldID).focus(); 
+  } 
+}
 /*input mask for mobile number*/
 document.getElementById('phone').addEventListener('input', function (e) {
   var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
