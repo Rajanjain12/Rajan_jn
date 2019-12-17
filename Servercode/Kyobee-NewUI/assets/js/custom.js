8570ffddc1cfaec42dad7223020844714285baca
Ruchi <ruchi.vakharia@ordextechnology.com>
2019-12-17 10:48:33 +0530

    committed for signup screen changes

--- Servercode/Kyobee-NewUI/assets/js/custom.js
+++ Servercode/Kyobee-NewUI/assets/js/custom.js
@@ -72,3 +72,7 @@ document.getElementById('phone').addEventListener('input', function (e) {
   var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
   e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
 });
+document.getElementById('storePhone').addEventListener('input', function (e) {
+  var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
+  e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
+});
