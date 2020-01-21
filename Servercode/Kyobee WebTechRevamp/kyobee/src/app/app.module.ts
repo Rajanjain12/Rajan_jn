955100a98da015ca909eb04a8ff78dd848b3955d
Fagun13Ordex <fagun.thakkar@ordextechnology.com>
2020-01-21 12:10:52 +0530

    commit as of 21/01/2020 -UI merge with filezila

--- Servercode/Kyobee WebTechRevamp/kyobee/src/app/app.module.ts
+++ Servercode/Kyobee WebTechRevamp/kyobee/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
