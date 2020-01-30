ed458cb8888a45b838f6b9c7100312bb138f3ca8
Fagun13Ordex <fagun.thakkar@ordextechnology.com>
2020-01-30 13:51:21 +0530

    commit as of 30/01/2020 -structure of wait list

---
+++ Servercode/Microservices/kyobeeWaitlistService/src/main/java/com/kyobeeWaitlistService/KyobeeWaitlistServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kyobeeWaitlistService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyobeeWaitlistServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyobeeWaitlistServiceApplication.class, args);
+	}
+
+}
---
+++ Servercode/Microservices/kyobeeWaitlistService/src/test/java/com/kyobeeWaitlistService/KyobeeWaitlistServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyobeeWaitlistService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyobeeWaitlistServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
