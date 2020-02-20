abe92199db10761042f9136d8b02f1c12ad42e67
Siddharth Jaiswal <sidjai09>
2020-02-20 14:35:29 +0530

    Commit for css console error fix

--- Servercode/Kyobee WebTechRevamp/kyobee/src/app/app.component.ts
+++ Servercode/Kyobee WebTechRevamp/kyobee/src/app/app.component.ts
@@ -23,42 +23,9 @@ export class AppComponent {
 
   /* Change Theme of Stylesheet */
   loadStyle(styleName: string) {
-    if (environment.production) {
-      // console.log(environment.production);
-
-      const themeLink = this.document.getElementById('client-theme') as HTMLLinkElement;
-      console.log('prod');
-      styleName += '.css';
-      themeLink.href = styleName;
-
-      // console.log(environment.production);
-
-      // const link0 = document.createElement('link');
-      // link0.href = 'styles.css';
-      // link0.rel = 'stylesheet';
-      // document.head.appendChild(link0);
-
-      // const link1 = document.createElement('link');
-      // link1.href = styleName + '.css';
-      // link1.rel = 'stylesheet';
-      // document.head.appendChild(link1);
-
-      // const link2 = document.createElement('link');
-      // link2.href = 'combine.css';
-      // link2.rel = 'stylesheet';
-      // document.head.appendChild(link2);
-    } else {
-      const script0 = document.createElement('script');
-      script0.src = 'styles.js';
-      document.head.appendChild(script0);
-
-      const script1 = document.createElement('script');
-      script1.src = styleName + '.js';
-      document.head.appendChild(script1);
-
-      const script2 = document.createElement('script');
-      script2.src = 'combine.js';
-      document.head.appendChild(script2);
-    }
+    console.log('______________ Prod', environment.production);
+    const themeLink = this.document.getElementById('client-theme') as HTMLLinkElement;
+    styleName += '.css';
+    themeLink.href = styleName;
   }
 }
--- Servercode/Kyobee WebTechRevamp/kyobee/src/environments/environment.ts
+++ Servercode/Kyobee WebTechRevamp/kyobee/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // serverUrl: "http://localhost:8082/"
-  serverUrl: 'http://localhost:8111/',
+  serverUrl: 'http://44.232.86.255:8081/',
+  // serverUrl: "http://localhost:8082/",
   pubnubSubscribeKey: 'sub-c-a408d714-2b8c-11ea-894a-b6462cb07a90',
   pubnubPublishKey: 'pub-c-cf81e7f1-47b0-4559-a127-720cd3085a92',
   pubnubIndividualChannel: 'RSNT_GUEST_DEV',
