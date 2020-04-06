bbdd0a5e1bc203b1382624bda3cd039df7821355
ria2610 <ria.sahesnani@ordextechnology.com>
2020-04-06 21:59:41 +0530

    - commit for rest template in user

--- Servercode/Microservices/kyobeeUserService/src/main/java/com/kyobeeUserService/KyobeeUserServiceApplication.java
+++ Servercode/Microservices/kyobeeUserService/src/main/java/com/kyobeeUserService/KyobeeUserServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import springfox.documentation.builders.PathSelectors;
@@ -35,5 +36,10 @@ public class KyobeeUserServiceApplication {
 	@LoadBalanced
 	public WebClient.Builder getWebClientBuilder(){
 		return WebClient.builder();
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+	    return new RestTemplate();
 	}
 }
