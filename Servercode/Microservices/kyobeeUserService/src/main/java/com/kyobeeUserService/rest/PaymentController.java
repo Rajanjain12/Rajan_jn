fe46383e1810012e25e387dae4b4179d9c0e06b5
ria2610 <ria.sahesnani@ordextechnology.com>
2020-06-11 18:03:36 +0530

    - changes for orgCardDetails API

--- Servercode/Microservices/kyobeeUserService/src/main/java/com/kyobeeUserService/rest/PaymentController.java
+++ Servercode/Microservices/kyobeeUserService/src/main/java/com/kyobeeUserService/rest/PaymentController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +27,7 @@ public class PaymentController {
 	private PaymentService paymentService;
 
 	@PostMapping(value = "/cardDetails", produces = "application/vnd.kyobee.v1+json")
-	public @ResponseBody ResponseDTO saveOrgCardDetails(@RequestParam Integer orgId, @RequestParam Integer customerId,
-			@RequestBody OrgCardDetailsDTO orgCardDetailsDTO) {
+	public @ResponseBody ResponseDTO saveOrgCardDetails(@RequestBody OrgCardDetailsDTO orgCardDetailsDTO) {
 
 		ResponseDTO responseDTO = new ResponseDTO();
 		try {
