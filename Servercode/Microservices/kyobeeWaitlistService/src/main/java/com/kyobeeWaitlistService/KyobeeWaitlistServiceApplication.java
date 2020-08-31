62122e7ad99758236d3cae75a013fbc83a9014c7
ria2610 <ria.sahesnani@ordextechnology.com>
2020-08-31 18:49:47 +0530

    - for waitlist service

--- Servercode/Microservices/kyobeeWaitlistService/src/main/java/com/kyobeeWaitlistService/KyobeeWaitlistServiceApplication.java
+++ Servercode/Microservices/kyobeeWaitlistService/src/main/java/com/kyobeeWaitlistService/KyobeeWaitlistServiceApplication.java
@@ -3,8 +3,8 @@ package com.kyobeeWaitlistService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;*/
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
